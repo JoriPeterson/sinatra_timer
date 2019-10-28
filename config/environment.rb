0d7c9ed47a0651181415793384789dde28055a4c
Jori Peterson <joripeterson@gmail.com>
2019-10-28 15:33:13 -0600

    Reconfigure config files

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,5 @@
 require 'bundler/setup'
 
-ENV['SINATRA_ENV'] ||= "development"
-ENV['RACK_ENV'] ||= "development"
-
 Bundler.require(:default, ENV['SINATRA_ENV'])
-RACK_ENV=production ruby app.rb
 
 require_all 'app'
-
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
