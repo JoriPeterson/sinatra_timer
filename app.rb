67f02b7f0b4ccd6bf7e0bc75e5980acfc10792ca
Jori Peterson <joripeterson@gmail.com>
2019-10-28 15:48:19 -0600

    Comment out domain

--- app.rb
+++ app.rb
@@ -11,12 +11,12 @@ configure do
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
-    :address => ENV['ADDRESS'],
-    :port => 1025,
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => ENV['MY_DOMAIN'],
+    # :domain => ENV['MY_DOMAIN'],
     :enable_starttls_auto => true
     }
   ActionMailer::Base.view_paths = File.expand_path('views/')
--- config/developement.rb
+++
@@ -1,2 +0,0 @@
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
