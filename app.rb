183e45bfcaceefd98544186643bd52cf523a46a8
Jori Peterson <joripeterson@gmail.com>
2019-10-28 15:29:50 -0600

    Change sendgrid variables

--- app.rb
+++ app.rb
@@ -11,12 +11,12 @@ configure do
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
+    :address => smtp.sendgrid.net,
     :port => 587,
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => ENV['SENDGRID_DOMAIN'],
+    :domain => ENV['MY_DOMAIN'],
     :enable_starttls_auto => true
     }
   ActionMailer::Base.view_paths = File.expand_path('views/')
