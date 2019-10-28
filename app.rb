0bab83d4a34de2f7aec531357cb405dc3451e499
Jori Peterson <joripeterson@gmail.com>
2019-10-28 17:15:46 -0600

    Move notify into app class

--- app.rb
+++ app.rb
@@ -5,13 +5,17 @@ class App < Sinatra::Base
   get "/" do
     erb :index
   end
+
+  get '/notify' do
+    Mailer.notify(params).deliver_now
+  end
 end
 
 configure do
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
-    :address => ENV['ADDRESS'],
+    :address => "smtp.sendgrid.net",
     :port => 587,
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
@@ -30,8 +34,4 @@ class Mailer < ActionMailer::Base
 		@params = params
 		mail(to: params[:email], subject: "Emergency Contact Notification: Action Needed")
 	end
-end
-
-get '/notify' do
-  Mailer.notify(params).deliver_now
 end
