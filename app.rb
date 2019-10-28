7b2aa8bda0e65e35dec024de7ad169ed6623b7ee
Jori Peterson <joripeterson@gmail.com>
2019-10-28 15:06:00 -0600

    Try address variables for production

--- app.rb
+++ app.rb
@@ -11,8 +11,8 @@ configure do
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
-    :address => ENV['ADDRESS'],
-    :port => 1025,
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
