1cd17ddd86403550b9cab3c98c378b4c9432b968
Jori Peterson <joripeterson@gmail.com>
2019-10-28 15:49:08 -0600

    Uncomment out domain

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ configure do
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    # :domain => ENV['MY_DOMAIN'],
+    :domain => ENV['MY_DOMAIN'],
     :enable_starttls_auto => true
     }
   ActionMailer::Base.view_paths = File.expand_path('views/')
